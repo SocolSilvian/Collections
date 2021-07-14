2808b6ed9dd108cf68b33d626d66d809da804bb8
Silviu <socolteodor@yahoo.com>
2021-07-14 22:19:07 +0300

    Add project files.

---
+++ Collections/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Collections
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void testbutton_Click(object sender, EventArgs e)
+        {
+            GenericClass t = new GenericClass(12, 13);
+            Console.WriteLine(t.Sum());
+        }
+    }
+}
---
+++ Collections/GenericClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class GenericClass
+    {
+        int a;
+        int b;
+
+        public GenericClass(int a = 0 ,int b = 0)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int Sum()
+        {
+            return a + b;
+        }
+    }
+}
